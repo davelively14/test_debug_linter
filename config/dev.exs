58079178e3a7afe2168d12fb96dfc20b9d9b48c2
Dave Lively <davelively14@gmail.com>
2017-04-26 18:07:46 -0400

    Configured postgres

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :test, Test.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "test_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :test, Test.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "test_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
